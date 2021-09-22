5d556fbeb86c3f666a8361a3f79580841e558337
piero <gagonzabe@gmail.com>
2021-09-22 17:11:51 -0500

    pipes

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent implements OnInit {
-  item_list:string;
+  itemList:string;
   constructor() { }
 
   ngOnInit(): void {
