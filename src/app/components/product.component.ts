8420958a539724eccdf0478a01de086f00392258
piero <gagonzabe@gmail.com>
2021-09-22 09:12:43 -0500

    inputs y outputs

---
+++ src/app/components/product.component.ts
@@ -0,0 +1,21 @@
+import { Component,Input, Output, EventEmitter } from '@angular/core';
+
+import {Product} from '../product.model';
+
+@Component({
+    selector : 'app-product',
+    templateUrl: './product.component.html'
+
+})
+export class ProductComponent{
+
+    @Input() product: Product; 
+    @Output() productClicked: EventEmitter<any>=new EventEmitter();
+
+
+    addCart(){
+        console.log('añadir al carrito');
+        this.productClicked.emit(this.product.id);
+    }
+
+}
