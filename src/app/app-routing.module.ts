2aa6cd3f3fd228991c5b776387b164c2c2de3fb4
piero <gagonzabe@gmail.com>
2021-09-23 12:09:21 -0500

    routeLink y activeRouteLink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,13 @@ import {HomeComponent} from './home/home.component';
 import {ProductsComponent} from './products/products.component';
 import { ContactComponent } from './contact/contact.component';
 import { DemoComponent } from './demo/demo.component';
-
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch:'full'
+  },
   {
     path: 'home',
     component: HomeComponent
@@ -21,6 +26,10 @@ const routes: Routes = [
   {
     path: 'demo',
     component: DemoComponent
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { DemoComponent } from './demo/demo.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BannerComponent } from './banner/banner.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { BannerComponent } from './banner/banner.component';
     DemoComponent,
     HeaderComponent,
     FooterComponent,
-    BannerComponent
+    BannerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
