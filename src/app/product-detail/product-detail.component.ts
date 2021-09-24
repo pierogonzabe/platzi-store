2197960c7bfa8250b3d0196556981b510f1ec160
piero <gagonzabe@gmail.com>
2021-09-24 12:02:40 -0500

    Creando pagina detalle producto

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router'
 import { ProductsService } from './../products.service'
-
+import {Product} from './../product.model'
 
 @Component({
   selector: 'app-product-detail',
@@ -10,16 +10,18 @@ import { ProductsService } from './../products.service'
 })
 export class ProductDetailComponent implements OnInit {
 
+  product: Product;
+
   constructor(
     private route: ActivatedRoute,
     private productsService: ProductsService
   ) { }
   
-  ngOnInit(): void {
+  ngOnInit(){
     this.route.params.subscribe((params: Params)=>{
       const id = params.id;
-      const product = this.productsService.getProduct(id)
-      console.log(product)
+      this.product = this.productsService.getProduct(id)
+      
     });
   }
 
