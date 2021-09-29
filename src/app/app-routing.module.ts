d25f86fe33713b8c4ec6184a209eec4499bfd9a1
piero <gagonzabe@gmail.com>
2021-09-29 12:51:37 -0500

    lazy loadig

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import {HomeComponent} from './home/home.component';
+import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
+
 import {ProductsComponent} from './products/products.component';
 import { ContactComponent } from './contact/contact.component';
 import { DemoComponent } from './demo/demo.component';
@@ -21,7 +21,7 @@ const routes: Routes = [
       },
       {
         path: 'home',
-        component: HomeComponent
+        loadChildren: ()=> import('./home/home.module').then(m=>m.HomeModule)
       },
       {
         path: 'products',
@@ -50,7 +50,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 import { BannerComponent } from './components/banner/banner.component';
-import { HomeComponent } from './home.component';
+import { HomeComponent } from './components/home/home.component';
 
 import { HomeRoutingModule } from './home-routing.module';
 
 @NgModule({
     declarations:[
         BannerComponent,
-        HomeComponent
-         
+        HomeComponent,
+        
     ],
     imports:[
-        HomeRoutingModule
+        HomeRoutingModule,
+        CommonModule
     ]
 })
 export class HomeModule{
